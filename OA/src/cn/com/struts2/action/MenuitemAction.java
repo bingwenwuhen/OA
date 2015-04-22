04ecf27c105e4264300517c28d279fa58e26e7f9
bingwenwuhen <bingwenwuhen@163.com>
2015-04-22 09:39:09 +0800

    将ajax与struts2进行了整合 建立了tree.html

--- OA/src/cn/com/struts2/action/MenuitemAction.java
+++ OA/src/cn/com/struts2/action/MenuitemAction.java
@@ -8,6 +8,8 @@ import org.apache.struts2.json.annotations.JSON;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
+import com.opensymphony.xwork2.ActionContext;
+
 import cn.com.domain.Menuitem;
 import cn.com.service.MenuitemService;
 import cn.com.struts2.action.base.BaseAction;
@@ -27,6 +29,6 @@ public class MenuitemAction extends BaseAction<Menuitem> {
 	@JSON(serialize=false)
 	public String getAllMenuitem(){
 		this.menuitemList=this.menuitemService.getAllMenuitem();
-		return null;
+		return SUCCESS;
 	}
 }
