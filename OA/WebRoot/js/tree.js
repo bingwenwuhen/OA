914a913b622c44a4deb8869d9f13ecab942c7378
bingwenwuhen <bingwenwuhen@163.com>
2015-04-22 20:34:24 +0800

    gif图片 部分jar包 插件的js文件

---
+++ OA/WebRoot/js/tree.js
@@ -0,0 +1,21 @@
+var tree={
+		setting:{
+			isSimpleData: true,
+			treeNodeKey: "mid",
+			treeNodeParentKey: "pid",
+			showLine: true,
+			root:{ 
+				isRoot:true,
+				nodes:[]
+			}
+		},
+	loadTree:function(){
+		$.post("menuitemAction_getAllMenuitem.action",null,function(data){
+			alert(data.menuitemList);
+			$("#tree").zTree(tree.setting,data.menuitemList);
+		});
+	}	
+};
+$().ready(function(){
+	tree.loadTree();
+});
